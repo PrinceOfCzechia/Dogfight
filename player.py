67263ff8082550f98248a2df2ad4978d729025a1
Petr Král <kralpet9@fjfi.cvut.cz>
2023-04-13 15:47:19 +0200

    increased player speed and rotation

--- player.py
+++ player.py
@@ -9,7 +9,7 @@ class Player:
         self.screen = screen
         self.angle = 270.0
         self.position = np.array( [ self.x, self.y ] )
-        self.speed = 0.2
+        self.speed = 0.3
         self.delta = np.array( [ np.cos(self.angle), np.sin(self.angle) ] ) * self.speed
         self.rotation = 0.0
         self.size = 32
@@ -21,19 +21,19 @@ class Player:
         self.screen.blit( rotated, ( self.x, self.y ) )
 
     def increment_speed( self ):
-        if self.speed <= 0.35: self.speed += 0.05
+        if self.speed <= 0.45: self.speed += 0.05
 
     def decrement_speed( self ):
-        if self.speed >= 0.15: self.speed -= 0.05
+        if self.speed >= 0.25: self.speed -= 0.05
 
     def get_rotation_increment( self ):
         # TODO: make this a linear function
-        if self.speed > 0.35: return 0.1
-        if self.speed > 0.3: return 0.11
-        if self.speed > 0.25: return 0.12
-        if self.speed > 0.2: return 0.13
-        if self.speed > 0.15: return 0.14
-        if self.speed > 0.1: return 0.15
-        else: return 0.14
+        if self.speed > 0.45: return 0.12
+        if self.speed > 0.4: return 0.15
+        if self.speed > 0.35: return 0.18
+        if self.speed > 0.3: return 0.20
+        if self.speed > 0.25: return 0.22
+        if self.speed > 0.2: return 0.25
+        else: return 0.30
 
 
