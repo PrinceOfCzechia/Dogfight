b027bcdaaf5e7e09a642f308d2e075ccbbcc7908
Petr Král <kralpet9@fjfi.cvut.cz>
2023-04-17 12:38:40 +0200

    finished HP counter functionality

--- player.py
+++ player.py
@@ -9,11 +9,12 @@ class Player:
         self.screen = screen
         self.angle = 270.0
         self.position = np.array( [ self.x, self.y ] )
-        self.speed = 0.3
+        self.speed = 0.7
         self.delta = np.array( [ np.cos(self.angle), np.sin(self.angle) ] ) * self.speed
         self.rotation = 0.0
         self.size = 32
-        self.hp = 5
+        self.max_hp = 5
+        self.hp = self.max_hp
         
     def draw( self ):
         rot_angle = 270 - self.angle
@@ -21,20 +22,20 @@ class Player:
         self.screen.blit( rotated, ( self.x, self.y ) )
 
     def increment_speed( self ):
-        if self.speed <= 0.45: self.speed += 0.05
+        if self.speed <= 0.85: self.speed += 0.05
 
     def decrement_speed( self ):
-        if self.speed >= 0.25: self.speed -= 0.05
+        if self.speed > 0.50: self.speed -= 0.05
 
     def get_rotation_increment( self ):
-        # TODO: make the magic number a function
-        if self.speed > 0.45: return 0.15
-        if self.speed > 0.4: return 0.18
-        if self.speed > 0.35: return 0.20
-        if self.speed > 0.3: return 0.22
-        if self.speed > 0.25: return 0.25
-        if self.speed > 0.2: return 0.35
-        else: return 0.40
+        # TODO: make the magic numbers a function
+        if self.speed > 0.65: return 0.50
+        if self.speed > 0.6: return 0.55
+        if self.speed > 0.55: return 0.60
+        if self.speed > 0.5: return 0.65
+        if self.speed > 0.45: return 0.70
+        if self.speed > 0.4: return 0.75
+        else: return 0.80
 
     def get_rect( self ):
         return pg.Rect( self.position, ( self.size, self.size ) )
@@ -47,4 +48,10 @@ class Player:
 
     def draw_hearts( self, full: pg.image, empty: pg.image, x_coordinate, y_coordinate ):
         for i in range( self.hp ):
-            self.screen.blit( full, (x_coordinate + 20 * i, y_coordinate ) )
+            self.screen.blit( full, ( x_coordinate - 20 * i, y_coordinate ) )
+        for i in range( self.max_hp - self.hp ):
+            self.screen.blit( empty, ( x_coordinate - 20 * ( self.max_hp - 1 - i ), y_coordinate ) )
+
+    def alive( self ):
+        if self.hp > 0: return True
+        else: return False
