dea73ed7294484b56bfc5b97c7730ca6f461ec0a
Petr Král <kralpet9@fjfi.cvut.cz>
2023-05-04 11:43:19 +0200

    simplified enemy.angle handling

--- enemy.py
+++ enemy.py
@@ -7,29 +7,32 @@ class Enemy:
         self.img = pg.transform.scale( pg.image.load( 'assets/tank.png' ), ( 32, 32 ) )
         self.screen = screen
         self.player = player
-        self.angle = 270.0
+        self.angle = 180.0
         self.position = np.array( [ x, y ] )
-        self.delta = np.array( [ np.cos(self.angle), np.sin(self.angle) ] )
+        self.delta = self.position - self.player.position / np.linalg.norm( self.position - self.player.position )
+        self.aim = np.array( [ np.cos(self.angle), np.sin(self.angle) ] )
         self.rotation_increment = 0.05
-        self.direction = 1
+        self.direction = -1
         self.size = 32
         self.rect = pg.Rect( self.position, ( self.size, self.size ) )
         self.dead = False
         
     def draw( self ):
-        rot_angle = 90 - self.angle
-        rotated = pg.transform.rotate( self.img, rot_angle )
+        rotated = pg.transform.rotate( self.img, self.angle )
         self.screen.blit( rotated, ( self.position[ 0 ], self.position[ 1 ] ) )
 
-    def correctDirection( self ):
-        if np.dot( self.player.delta, self.delta > 0 ): return True
+    def correct_direction( self ):
+        if np.dot( self.aim, self.delta > 0 ): return True
         else: return False
 
     def kill( self ):
         self.dead = True
 
-    def changeDirection( self ):
+    def change_direction( self ):
         self.direction = self.direction * -1
 
     def rotate( self ):
         self.angle += self.direction * self.rotation_increment
+
+    def update_aim( self ):
+        self.aim = np.array( [ np.cos(self.angle), np.sin(self.angle) ] )
