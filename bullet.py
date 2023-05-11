7896ca984495096f19bd2da0620b9d2801933072
Petr Král <kralpet9@fjfi.cvut.cz>
2023-05-11 14:24:27 +0200

    related the bullet to player.center

--- bullet.py
+++ bullet.py
@@ -4,12 +4,12 @@ from player import Player
 
 class Bullet:
     def __init__( self, screen, player: Player ):
-        self.x = player.position[ 0 ] + 8
-        self.y = player.position[ 1 ] + 8
+        self.x = player.center[ 0 ]
+        self.y = player.center[ 1 ]
         self.img = pg.transform.rotate(
                    pg.transform.scale(
                    pg.image.load( 'assets/bullet.png' ), ( 6, 6 ) ),
-                   270 - player.angle )
+                   270.0 - player.angle )
         self.screen = screen
         self.position = np.array( [ self.x, self.y ] )
         self.delta = np.copy( player.delta ) / np.linalg.norm( player.delta )
@@ -17,7 +17,7 @@ class Bullet:
 
     def draw( self ):
         self.screen.blit( self.img, self.position )
-        pg.draw.rect( self.screen, [255,0,0], self.get_rect() )
+        # pg.draw.rect( self.screen, [255,0,0], self.get_rect() ) TODO remove when done
 
     def get_rect( self ):
         return pg.Rect( self.position[ 0 ], self.position[ 1 ], 2, 2 )
