fixed disparity of drawing and rotation angle

enemy.py
@@ -8,52 +8,69 @@ class Enemy:
         self.screen = screen
         self.player = player
         self.angle = 180.0
-        self.draw_angle = 180
+        self.draw_angle = 180 / np.pi + 90
         self.position = np.array( [ x, y ] )
-        self.delta = ( self.player.position - self.position ) / np.linalg.norm( self.player.position - self.position )
-        self.aim = np.array( [ np.cos(self.angle), np.sin(self.angle) ] )
-        self.perp = np.array( [ self.aim[1], -self.aim[0] ] )
-        self.dot_1 = np.dot( self.delta, self.aim )
-        self.dot_2 = np.dot( self.delta, self.perp )
-        self.aim_rect = pg.Rect( self.position[0] + 100 * self.aim[0], self.position[1] + 100 * self.aim[1], 10, 10 )
-        self.delta_rect = pg.Rect( self.position[0] + 100*self.delta[0], self.position[1] + 100*self.delta[1], 10, 10 )
-        self.perp_rect = pg.Rect( self.position[0] + 100 * self.perp[0], self.position[1] + 100 * self.perp[1], 10, 10 )
+        self.center = np.array( [ x+8, y+8 ] )
+        self.delta = ( self.player.position - self.center ) / np.linalg.norm( self.player.position - self.center )
+        self.aim = np.array( [ np.cos(self.angle-90), np.sin(self.angle-90) ] )
+        self.backward = -1 * self.aim
+        self.aim_rect = pg.Rect( self.center[0] + 100 * self.aim[0], self.center[1] + 100 * self.aim[1], 10, 10 )
+        self.delta_rect = pg.Rect( self.center[0] + 100*self.delta[0], self.center[1] + 100*self.delta[1], 10, 10 )
+        self.backward_rect = pg.Rect( self.center[0] + 100 * self.backward[0],
+                                      self.center[1] + 100 * self.backward[1],
+                                      5, 5 )
         self.rotation_increment = 0.1
-        self.direction = 1
+        self.direction = -1
         self.size = 32
         self.rect = pg.Rect( self.position, ( self.size, self.size ) )
         self.dead = False
         self.cooldown = 0.2
+        self.forward_collision = False
+        self.backward_collision = False
         
     def draw( self ):
         rotated = pg.transform.rotate( self.img, self.draw_angle )
         self.screen.blit( rotated, ( self.position[ 0 ], self.position[ 1 ] ) )
         pg.draw.rect( self.screen, [255,0,0], self.aim_rect )
         pg.draw.rect( self.screen, [0,0,255], self.delta_rect )
-        pg.draw.rect( self.screen, [0,255,0], self.perp_rect )
+        pg.draw.rect( self.screen, [0,255,0], self.backward_rect )
 
     def kill( self ):
         self.dead = True
-
-    def correct_direction( self ):
-        if self.dot_1 > 0 and self.dot_2 > 0: return True
-        else: return False
 
     def correct_aim( self ):
         if pg.Rect.colliderect( self.aim_rect, self.delta_rect ): return True
         else: return False
 
+    def check_change( self ):
+        if pg.Rect.colliderect( self.delta_rect, self.backward_rect ):
+            self.backward_collision = True
+        else:
+            if self.backward_collision: self.change_direction()
+            self.backward_collision = False
+        '''
+        if self.correct_aim():
+            self.forward_collision = True
+        else:
+            if self.forward_collision: self.change_direction()
+            self.forward_collision = False
+        '''
+
     def change_direction( self ):
-        self.direction = self.direction * -1      
+        self.direction = self.direction * -1
             
     def update( self ):
-        self.draw_angle += self.rotation_increment * self.direction
+        self.draw_angle -= self.rotation_increment * self.direction
         self.angle += self.rotation_increment * np.pi / 180 * self.direction
-        self.aim = [ np.cos( self.angle ), np.sin( self.angle ) ]
-        self.delta = ( self.player.position - self.position ) / np.linalg.norm( self.player.position - self.position )
-        self.perp = np.array( [ self.aim[1], -self.aim[0] ] )
-        self.dot_1 = np.dot( self.delta, self.aim )
-        self.dot_2 = np.dot( self.delta, self.perp )
-        self.aim_rect = pg.Rect( self.position[0] + 100*self.aim[0], self.position[1] + 100*self.aim[1], 10, 10 )
-        self.delta_rect = pg.Rect( self.position[0] + 100*self.delta[0], self.position[1] + 100*self.delta[1], 10, 10 )
-        self.perp_rect = pg.Rect( self.position[0] + 100 * self.perp[0], self.position[1] + 100 * self.perp[1], 10, 10 )
+        self.aim = [ np.cos( self.angle-90 ), np.sin( self.angle-90 ) ]
+        self.backward = [ -np.cos( self.angle-90 ), -np.sin( self.angle-90 ) ]
+        self.delta = ( self.player.position - self.center ) / np.linalg.norm( self.player.position - self.center )
+        self.aim_rect = pg.Rect( self.center[0] + 100*self.aim[0],
+                                 self.center[1] + 100*self.aim[1],
+                                 10, 10 )
+        self.delta_rect = pg.Rect( self.center[0] + 100*self.delta[0],
+                                   self.center[1] + 100*self.delta[1],
+                                   10, 10 )
+        self.backward_rect = pg.Rect( self.center[0] + 100 * self.backward[0],
+                                      self.center[1] + 100 * self.backward[1],
+                                      5, 5 )